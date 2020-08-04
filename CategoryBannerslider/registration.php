0aa491b56a03f44af084a8ccbd302615a91c9241
Riddhi Dhakate <riddhi.dhakate@brainvire.com>
2020-08-04 14:56:29 +0530

    remove zneddesk ticket using m2

--- CategoryBannerslider/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Brainvire_CategoryBannerslider',
-    __DIR__
-);
--- CategoryBannerslider/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            owlcarouselslider: 'Brainvire_CategoryBannerslider/js/owl.carousel'
-        }
-    }
-};
